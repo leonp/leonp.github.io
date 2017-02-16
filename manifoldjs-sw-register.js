50b3776935a8732a4c539b416287c4cd045805c4
Leon Paternoster <leon.paternoster@zoho.com>
2017-02-16 11:22:46 +0000

    Manifest change

--- manifoldjs-sw-register.js
+++ manifoldjs-sw-register.js
@@ -6,7 +6,7 @@ if (navigator.serviceWorker.controller) {
 } else {
 
 //Register the ServiceWorker
-  navigator.serviceWorker.register('manifoldjs-sw.js', {
+  navigator.serviceWorker.register('/manifoldjs-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
